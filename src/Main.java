10d4ef7a860186f3c5f69b1dd48550b8a6d4d479
YounGyoungMo <gyoungmo2011@naver.com>
2025-03-07 13:34:36 +0900

    level2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-import java.util.InputMismatchException;
-
-import java.util.List;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
